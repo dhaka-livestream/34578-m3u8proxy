9d27b2cfc40dcec81b8ab3d21cc1e68af5c3c515
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-16 05:35:56 +0600

    Add new streaming URL for Andtvhd

--- _worker.js
+++ _worker.js
@@ -173,6 +173,10 @@ export default {
         url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeebollywood/default/manifest.mpd',
         type: 'redirect'
       },
+      'zee5-andtvhd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andtvhd/default/index.mpd',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
