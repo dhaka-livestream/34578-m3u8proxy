4ebd360eb577e5cc281e6621b78d15c745b95d76
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-16 05:09:17 +0600

    Add new Zee5 channels with redirect type

--- _worker.js
+++ _worker.js
@@ -137,6 +137,14 @@ export default {
         url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andpictureshd/default/index.mpd',
         type: 'redirect'
       },
+      'zee5-zeetvhd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeetvhd/default/index.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeecinemahd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeecinemahd/default/manifest.mpd',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
@@ -152,10 +160,6 @@ export default {
       'andpicture_hd': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/andpicture_hd/playlist.m3u8',
         type: 'proxy'
-      },
-      'zee5-zeetvhd': {
-        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeetvhd/default/index.mpd',
-        type: 'redirect'
       },
       'mtv': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/mtv_576/mtv_576.m3u8?bitrate=500000&channel=mtv_576&gp_id=',
