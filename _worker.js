2477254ab451c26991d2edc7e245e07a4edbc002
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-16 05:18:51 +0600

    Add Zee Cinema redirect URL to _worker.js

--- _worker.js
+++ _worker.js
@@ -153,6 +153,10 @@ export default {
         url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeetv/default/manifest.mpd',
         type: 'redirect'
       },
+      'zee5-zeecinema': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeecinema/default/manifest.mpd',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
