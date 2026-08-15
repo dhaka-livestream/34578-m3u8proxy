088130f4e068059a99dcf4448443b441547954e5
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-16 05:50:44 +0600

    Add new redirect for Zee5 Zoom

--- _worker.js
+++ _worker.js
@@ -189,6 +189,10 @@ export default {
         url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeeaction/default/main.mpd',
         type: 'redirect'
       },
+      'zee5-zoom': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zoom2/default/manifest.mpd',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
