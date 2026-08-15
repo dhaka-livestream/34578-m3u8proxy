d8a29ecc2c100b7c46f8ed938c71dbba64af9ae6
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-16 05:43:42 +0600

    Add Zee5 Zing redirect URL to _worker.js

--- _worker.js
+++ _worker.js
@@ -181,6 +181,10 @@ export default {
         url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andtv/default/manifest.mpd',
         type: 'redirect'
       },
+      'zee5-zing': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zing/default/manifest.mpd',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
