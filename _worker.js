e657b591fb6144529ce8fdee679fcaf436ac8866
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-18 01:14:48 +0600

    Add new infotainment-adventureearth stream

--- _worker.js
+++ _worker.js
@@ -217,6 +217,10 @@ export default {
         url: 'http://live.balajibroadband.com:3500/live/3444.m3u8',
         type: 'redirect'
       },
+      'infotainment-adventureearth': {
+        url: 'https://autentic-adventure-earth-1-eu.rakuten.wurl.tv/playlist.m3u8',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
